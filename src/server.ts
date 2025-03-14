5e811415ea095d87b7e7a120c8e69165f03475a9
leirisson <leirissonsouza99@gmail.com>
2025-03-13 21:25:39 -0400

    Enviando requisição com o método GET

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ const PORT = 3334
 
 const app = express()
 
+app.get('/', (request, response) => {
+    response.send('hello express')
+})
+
 app.listen(PORT, () => {
     console.log('Servidor está funcionando.')
 })
