66431b5ed366c9b5f54c45d00fdbdbb03198d532
leirisson <leirissonsouza99@gmail.com>
2025-03-13 21:53:48 -0400

    dados no corpo da requisição

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,32 @@
 import express from 'express'
+import { report } from 'process'
 
 const PORT = 3334
 
 const app = express()
+app.use(express.json())
 
+
+// parametros não nomeados
 app.get('/produtos/:id', (request, response) => {
     const {id} = request.params
     response.send(`produto id: ${id}`)
+})
+
+// parametros nomeados
+app.get('/produtos', (request, response) => {
+    //  /produtos?page=1&limit=10
+    const {page, limit} = request.query
+
+    response.send(`Pagina: ${page} de ${limit}`)
+})
+
+
+
+app.post('/produtos', (request, resposne) => {
+    const {name, price} = request.body
+
+    resposne.status(201).json({name, price})
 })
 
 app.listen(PORT, () => {
