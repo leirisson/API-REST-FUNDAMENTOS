fe686f4645863e0645480893432151be345fc7c4
leirisson <103783610+leirisson@users.noreply.github.com>
2025-03-17 05:46:08 -0400

    deletando pastas não ultilizadas

--- src/middlewares/my-middleware.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction} from 'express'
-
-export function MyMiddleware(request: Request, response: Response, next: NextFunction){
-    console.log('passou pelo middlware global')
-
-    return next()
-}
--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { protudosRoutes } from "./produtos-routes";
-
-const routes = Router()
-
-routes.use('/produtos', protudosRoutes)
-
-
-export { routes }
