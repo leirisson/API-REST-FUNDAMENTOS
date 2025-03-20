3adcb9870737079f9aca8c82bf38279904be8b96
leirisson <103783610+leirisson@users.noreply.github.com>
2025-03-19 23:58:24 -0400

    ADCIONANDO EXPLICAÇÃO

--- @projetos-exemplos/3-Middleware-Autenticacao-Autorizacao/src/middleware/authMiddleware .ts
+++ @projetos-exemplos/3-Middleware-Autenticacao-Autorizacao/src/middleware/authMiddleware .ts
@@ -4,8 +4,11 @@ import { Request, Response, NextFunction } from "express";
  * Na requisição com a autorização do tipoe: API Key
  * é preciso mandar a Key: chave_autencicacao
  * é preciso mandar o valor da chave que é: meu-token-secreto
- * 
  * caso não senha essa chave ou o valor estaja errado vai cair no não autorizado !
+ * 
+ * EXPLICAÇÃO 2:
+ * Neste exemplo, o middleware authMiddleware verifica se o token enviado no cabeçalho da requisição é igual a 'meu-token-secreto'. 
+ * Se for, o usuário é considerado autenticado e pode acessar a rota /protegido. Caso contrário, ele recebe uma resposta 401 Não autorizado.
  */
 
 
