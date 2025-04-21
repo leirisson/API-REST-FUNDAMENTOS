4db9d3b9341d3b4f6951fc2bb0169f79b96eaa36
leirisson <leirissonsouza99@gmail.com>
2025-04-20 20:11:47 -0400

    subir

--- 18-query-builder/9-api-restaurant/knexfile.ts
+++ 18-query-builder/9-api-restaurant/knexfile.ts
@@ -1,7 +1,24 @@
 export default {
     client: "sqlite3",
-    connection :  {
-        filename : "./src/database/database.db"
+    connection: {
+        filename: "./src/database/database.db"
     },
-    
+    // habilitando as retições de relacionamento 
+    pool: {
+        afterCreate: (connection: any, done: any) => {
+            connection.run("PRAGMA foreign_keys = ON")
+            done()
+        },
+    },
+    useNullAsDefault: true,
+    migrations: {
+        extensions: "ts",
+        directory: "./src/database/migrations"
+    },
+    seeds: {
+        extensions: "ts",
+        directory: "./src/database/seeds"
+    }
+
+
 }
