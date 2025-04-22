b6dd28496a30497c7f04c9438bbd54dead836674
leirisson <leirissonsouza99@gmail.com>
2025-04-21 21:25:34 -0400

    subir

---
+++ @projetos-exemplos/@-typescript/1.Calculando-Idade/index.ts
@@ -0,0 +1,14 @@
+
+
+const usuario = {
+    anoQueNasceu: 1999,
+}
+
+
+function calculandoIdade(usuario: Usuario) {
+    return console.log(2025 - usuario.anoQueNasceu)
+}
+
+
+
+calculandoIdade(usuario)
