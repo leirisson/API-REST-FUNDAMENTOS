509b65496a1605d6af381bcc12a9632a664ee969
leirisson <leirissonsouza99@gmail.com>
2025-04-24 02:34:14 -0400

    exibindo todos os usuarios cadastrados, com a rota da api

--- @projetos-exemplos/11-api-de-tarefas/src/controllers/users-controller.ts
+++ @projetos-exemplos/11-api-de-tarefas/src/controllers/users-controller.ts
@@ -1,10 +1,14 @@
+import { knex } from "@/database/knex"
 import { Request, Response, NextFunction } from "express"
 
 
 export class UserController {
     async index(request: Request, response: Response, next: NextFunction) {
         try {
-            response.json({message: "ok"})
+
+            const users = await knex<UserRepository>("users")
+            
+            response.json({users})
         } catch (error) {
             next(error)
         }
