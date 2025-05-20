51c4aae736d0f5b2ea445b91cd0429985681b997
leirisson <leirissonsouza99@gmail.com>
2025-05-19 20:50:44 -0400

    iniciando o projeto

---
+++ 19-APIs-com-ORM/2 -express/2-API-consultas-médicas/src/server.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import { app } from "./app"
+import express, { Request, Response } from "express"
+
+app.use(express.json())
+
+const PORT = process.env.PORT
+
+
+app.get("/", (request: Request, response: Response) => {
+    response.send("<h1 style='padding:1rem;'>API de consultas medicas</h1>")
+})
+
+app.listen(PORT, () => console.log(`servidor funcionando em http://localhost:${PORT}`))
