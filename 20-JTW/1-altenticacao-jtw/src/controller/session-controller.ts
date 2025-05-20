a475efe334e56b57cbd3fd3feb3f172a6531e5a3
leirisson <leirissonsouza99@gmail.com>
2025-05-20 16:50:38 -0400

    tratando erros

--- 20-JTW/1-altenticacao-jtw/src/controller/session-controller.ts
+++ 20-JTW/1-altenticacao-jtw/src/controller/session-controller.ts
@@ -1,8 +1,26 @@
-import {Request, Response, NextFunction} from 'express'
+import { Request, Response } from 'express'
+import { AppError } from '../utils/AppError'
+
 
 
 export class SessionsController {
-    async create(request:Request, response:Response, next:NextFunction){
-        response.json({msg:'teste'})
+    async create(request: Request, response: Response) {
+
+        // recuperando os dados
+        const { username, password } = request.body
+
+        const fakeUser = {
+            id: 1,
+            username: "leirisson",
+            password: "123456"
+        }
+
+        if (username !== fakeUser.username || password !== fakeUser.password) {
+            throw new AppError("E-mail  e/ou senha incorreta", 401)
+        }
+
+        
+
+        response.json({ msg: 'teste' })
     }
 }
